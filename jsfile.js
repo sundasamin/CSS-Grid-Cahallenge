1166327de3a1ce27ecf59ef1465953e6bc614406
Sundas <sundusamin@yahoo.com>
2023-02-16 23:36:12 +0500

    files updated

---
+++ jsfile.js
@@ -0,0 +1,7 @@
+function togglenav(){
+    const navExpand= document.querySelector(".container");
+    navExpand.classList.toggle("nav-expand");
+    const hideNav= document.querySelector("nav");
+    hideNav.classList.toggle("nav-hide")
+
+}
